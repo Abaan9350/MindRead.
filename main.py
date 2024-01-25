b9792eb3d1692eed2f2577990eb18640f0eca776
Abaan9350 <139568623+Abaan9350@users.noreply.github.com>
2024-01-25 16:00:42 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+option = input("Is your character a boy?\n")
+if option == "yes":
+    option = input("Does your character have a cat?\n")
+    if option == "yes":
+        option = input("Does your character invest in stock market?\n")
+
