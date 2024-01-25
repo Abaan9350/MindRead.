d15eb0d07794e3947a1cf414dcfbcabb429fa6e8
Abaan9350 <139568623+Abaan9350@users.noreply.github.com>
2024-01-25 16:18:57 +0530

    Added 6 students

--- main.py
+++ main.py
@@ -1,6 +1,41 @@
+#Raihan, Vedant Shetty, Ganesh, Abaan, Aryan, Dipesh
 option = input("Is your character a boy?\n")
 if option == "yes":
-    option = input("Does your character have a cat?\n")
-    if option == "yes":
-        option = input("Does your character invest in stock market?\n")
 
+    option = input("Does he wear spectacles?\n")
+    if option == "yes":
+        option = input("Is he scolded in class?\n")
+        if option == "yes":
+            option = input("Does he invest in stock market?\n")
+            if option == "yes":
+                option = input("Does he have a cat?\n")
+                if option == "yes":
+                    option = input("You are thinking of Raihan!")
+                else:
+                    option = input("Is he good at driving?\n")
+                    if option == "yes":
+                        option = input("You are thinking of Vedant Shetty")
+                    else:
+                        option = input("You are thinking of Ganesh!")
+
+    option = input("Does he get scoleded in class?\n")
+    if option == "yes":
+        option = input("Does he wear shirts usually? \n")
+        if option == "yes":
+            option = input("")
+        else:
+            option = input("Is he good at football?\n")
+            if option == "yes":
+                option = input("Is he known for his hair?\n")
+                if option == "yes":
+                    option = input("You are thinking of Aryan!")
+                else:
+                    option = input("Is his walk iconic?\n")
+                    if option == "yes":
+                        option = input("You are thinking of Dipesh!\n")
+                    else:
+                        option = input("You are thinking of Abaan!\n")
+            else:
+                option = input("Is he known for his hair?\n")
+                if option == "yes":
+                    option = input("You are thinking of Aryan!")
