435d5ef8185f6ff1db5186cadc91b05a44989735
Abaan9350 <139568623+Abaan9350@users.noreply.github.com>
2024-02-03 22:13:33 +0530

    Fixed major bugs 1.0

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 
 option = input("Is your character a boy?\n")
 if option == "yes":
-
     option = input("Does he wear spectacles?\n")
     if option == "yes":
         option = input("Is he scolded in class?\n")
@@ -18,71 +17,49 @@ if option == "yes":
                         print("You are thinking of Vedant Shetty")
                     else:
                         print("You are thinking of Ganesh!")
+
+    else:
+        option = input("Does he get scolded in class?\n")
+        if option == "yes":
+            option = input("Does he wear shirts usually? \n")
+            if option == "yes":
+                option = input("")
             else:
-                option = input("Does he live in Nerul?\n")
+                option = input("Is he good at football?\n")
                 if option == "yes":
-                    print("You are thinking of Chaskar!")
+                    option = input("Is he known for his hair?\n")
+                    if option == "yes":
+                        print("You are thinking of Aryan!")
+                    else:
+                        option = input("is he South Indian?\n")
+                        if option == "yes":
+                            print("You are thinking of Dipesh!\n")
+                        else:
+                            print("You are thinking of Abaan!\n")
                 else:
+                    option = input("Is he known for his hair?\n")
+                    if option == "yes":
+                        print("You are thinking of Aryan!")
+        else:
+            option = input("Does he help everyone with assignments?\n")
+            if option == "yes":
+                print("You are thinking of Sameer!")
+            else:
+                option = input("Does he have interest in cameras?\n")
+                if option == "yes":
                     option = input("Is he good at video games?\n")
                     if option == "yes":
-                        print("You are thinking of Nidhish!")
-                    else:
-                        option = input("Does he sleep in class?\n")
+                        print("You are thinking of Amruthesh!")
+                else:
+                    option = input("Good at video games?\n")
+                    if option == "yes":
+                        option = input("Autistic?\n")
                         if option == "yes":
-                            print("You are thinking of Anish Masurkar!")
-                        else:
-                            option = input("Does he wear shirts frequently?\n")
-
-        else: #not scolded
-            option = input("Is he good at football?\n")
-            if option == "yes":
-                print("You are thinking of Soham Desai!")
-            else:
-                option = input("Is he good at maths?\n|")
-                if option == "yes":
-                    print("You are thinking of Ashish!")
-
-    option = input("Does he get scoleded in class?\n")
-    if option == "yes":
-        option = input("Does he wear shirts usually? \n")
-        if option == "yes":
-            option = input("")
-        else:
-            option = input("Is he good at football?\n")
-            if option == "yes":
-                option = input("Is he known for his hair?\n")
-                if option == "yes":
-                    print("You are thinking of Aryan!")
-                else:
-                    option = input("Is his walk iconic?\n")
-                    if option == "yes":
-                        print("You are thinking of Dipesh!\n")
+                            print("You are thinking of Uday!")
                     else:
-                        print("You are thinking of Abaan!\n")
-            else:
-                option = input("Is he known for his hair?\n")
-                if option == "yes":
-                    print("You are thinking of Aryan!")
-    else:
-        option = input("Does he help everyone with assignments?\n")
-        if option == "yes":
-            print("You are thinking of Sameer!")
-        else:
-            option = input("Does he have interest in cameras?\n")
-            if option == "yes":
-                option = input("Is he good at video games?\n")
-                if option == "yes":
-                    print("You are thinking of Amruthesh!")
-            else:
-                option = input("Good at video games?\n")
-                if option == "yes":
-                    option = input("Autistic?\n")
-                    if option == "yes":
-                        print("You are thinking of Uday!")
-                else:
-                    option = input("Is he really good at football?\n")
-                    if option == "yes":
-                        print("You are thinking of Sai!")
+                        option = input("Is he really good at football?\n")
+                        if option == "yes":
+                            print("You are thinking of Sai!")
                         
 
 else: #this is for lediz
@@ -137,11 +114,3 @@ else: #this is for lediz
                 option = input("Extrovert?\n")
                 if option == "yes":
                     print("You are thinking of Archi!")
-
-
-
-
-
-
-
-
